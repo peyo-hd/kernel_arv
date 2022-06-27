58ea5149a08a35096db67065330dcbe342f74e91
Peter Yoon <peyo.hd@gmail.com>
2022-06-27 17:48:36 +0900

    net: block bpf running
      to avoid kernel crash by bpf

--- net/core/filter.c
+++ net/core/filter.c
@@ -120,9 +120,6 @@ EXPORT_SYMBOL_GPL(copy_bpf_fprog_from_user);
  */
 int sk_filter_trim_cap(struct sock *sk, struct sk_buff *skb, unsigned int cap)
 {
-	int err;
-	struct sk_filter *filter;
-
 	/*
 	 * If the skb was allocated from pfmemalloc reserves, only
 	 * allow SOCK_MEMALLOC sockets to use it as this socket is
@@ -132,28 +129,8 @@ int sk_filter_trim_cap(struct sock *sk, struct sk_buff *skb, unsigned int cap)
 		NET_INC_STATS(sock_net(sk), LINUX_MIB_PFMEMALLOCDROP);
 		return -ENOMEM;
 	}
-	err = BPF_CGROUP_RUN_PROG_INET_INGRESS(sk, skb);
-	if (err)
-		return err;
 
-	err = security_sock_rcv_skb(sk, skb);
-	if (err)
-		return err;
-
-	rcu_read_lock();
-	filter = rcu_dereference(sk->sk_filter);
-	if (filter) {
-		struct sock *save_sk = skb->sk;
-		unsigned int pkt_len;
-
-		skb->sk = sk;
-		pkt_len = bpf_prog_run_save_cb(filter->prog, skb);
-		skb->sk = save_sk;
-		err = pkt_len ? pskb_trim(skb, max(cap, pkt_len)) : -EPERM;
-	}
-	rcu_read_unlock();
-
-	return err;
+	return 0;
 }
 EXPORT_SYMBOL(sk_filter_trim_cap);
 
--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -2029,14 +2029,6 @@ static unsigned int run_filter(struct sk_buff *skb,
 			       const struct sock *sk,
 			       unsigned int res)
 {
-	struct sk_filter *filter;
-
-	rcu_read_lock();
-	filter = rcu_dereference(sk->sk_filter);
-	if (filter != NULL)
-		res = bpf_prog_run_clear_cb(filter->prog, skb);
-	rcu_read_unlock();
-
 	return res;
 }
 
